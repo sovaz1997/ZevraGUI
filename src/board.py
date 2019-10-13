54ed991bc362804377805c98951664ce34be28bc
Oleg Smirnov <sovaz1997@yandex.ru>
2019-10-13 17:12:57 +0300

    отрисовка доски цветами

--- src/board.py
+++ src/board.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QWidget
-from PyQt5.QtGui import QPainter
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 
 import chess
 
@@ -19,7 +19,17 @@ class Board(QWidget):
         qp = QPainter();
         qp.begin(self)
         cellSize = self.size / 8;
+
+        whiteCell = QColor(240, 217, 181)
+        blackCell = QColor(181, 136, 99)
+
+        qp.setPen(QColor(0, 0, 0, 0))
         for i in range(8):
             for j in range(8):
+                if (i + j) % 2 == 0:
+                    qp.setBrush(blackCell)
+                else:
+                    qp.setBrush(whiteCell)
+
                 qp.drawRect(i * cellSize, j * cellSize, cellSize, cellSize)
         qp.end()
