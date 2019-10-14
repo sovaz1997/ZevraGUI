6839a5095c5acec1092540c356179bbd9ac3ca75
Oleg Smirnov <sovaz1997@yandex.ru>
2019-10-14 10:39:24 +0300

    refactor

--- src/zevragui.py
+++ src/zevragui.py
@@ -7,24 +7,14 @@ from gamemodule import GameView
 class ZevraGUI(QMainWindow):
     def __init__(self):
         super().__init__()
-
         self.game = GameView(self, 50)
-
         self.initUI()
 
     def initUI(self):
-        #self.centralwidget = QWidget(self)
-        #self.setCentralWidget(self.centralwidget)
-        
         self.setWindowTitle('Zevra GUI')
         self.resize(1280, 720)
 
         self.setCentralWidget(self.game)
-
-        #layout = QVBoxLayout(self.centralWidget())
-        
-        #layout.addWidget(self.game)
-
         self.game.setFocus()
 
         self.show()
